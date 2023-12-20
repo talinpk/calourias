932ff4d4fe79e56e7b7b6c21bccc903a01f99b8e
tripaseca <almanegra@email.com>
2023-12-20 00:51:53 -0300

    Initial Commit

---
+++ calourias/calouriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  calouriasApp.swift
+//  calourias
+//
+//  Created by ifpb on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct calouriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
